c10c3240357da5b130a4b020e1eaa2bb1aeb7d9c
bripui <britta.puyn@outlook.de>
2021-07-05 22:53:43 +0200

    implemented session and restart option

--- scraper/api_bgg_scraper.py
+++ scraper/api_bgg_scraper.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 from bs4 import BeautifulSoup
 import random
 import time
@@ -11,6 +13,19 @@ def calculate_pagecount(total_items):
     else:
         pages = (int(total_items)//100)+1
     return pages
+
+retry_strategy = Retry(
+    total=10, 
+    backoff_factor=1,
+    respect_retry_after_header=True,
+    status_forcelist=[429, 413, 502, 500],
+)
+adapter = HTTPAdapter(max_retries=retry_strategy)
+session = requests.Session()
+session.mount("https://", adapter)
+session.mount("http://", adapter)
+# use the session object to make requests
+#response = session.get(url)
 
 logging.basicConfig(filename='api_scraper.log', 
                     level=logging.INFO)
@@ -24,9 +39,13 @@ boardgames['integrations'] = None
 boardgames['designers'] = None
 boardgames['publishers'] = None
 
+restart = True
+restart_page = 297
+restart_id = 30549
+
 sleep_default = 2
 
-for ID in boardgames.index:
+for ID in boardgames.loc[restart_id:].index:
     logging.info(f'movie id: {ID}')
     
     user_ratings = []
@@ -34,10 +53,12 @@ for ID in boardgames.index:
     comments = []
     ratings = pd.DataFrame()
     page = 1
+
     
+        
     # request page 1 of game overview with user ratings and comments
     api_adress = f"https://www.boardgamegeek.com/xmlapi2/thing?id={ID}&ratingcomments=1&page={page}"
-    response = requests.get(api_adress)  
+    response = session.get(api_adress)  
     logging.info(f'status {response.status_code} for {api_adress}')    
     
     xml = response.text
@@ -50,86 +71,129 @@ for ID in boardgames.index:
     logging.info(f'pagecount: {pagecount}')
     logging.info(f'start scraping page {page} of {pagecount}')
 
-    
-    # extend boardgames dataframe 
-    categories = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgamecategory'})]
-    boardgames.loc[ID,'categories'] = ', '.join(categories)
-    mechanics = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgamemechanic'})]
-    boardgames.loc[ID,'mechanics'] = ', '.join(mechanics)
-    family = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgamefamily'})]
-    boardgames.loc[ID,'family'] = ', '.join(family)
-    expansions = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgameexpansion'})]
-    boardgames.loc[ID,'expansions'] = ', '.join(expansions)
-    integrations = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgameintegration'})]
-    boardgames.loc[ID,'integrations'] = ', '.join(integrations)
-    designers = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgamedesigner'})]
-    boardgames.loc[ID,'designers'] = ', '.join(designers)
-    publishers = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgamepublisher'})]
-    boardgames.loc[ID,'publishers'] = ', '.join(publishers)
-    
-    # update number of voters
-    boardgames.loc[ID, 'num_voters'] = total_items
-    
-    boardgames.to_csv('../data/boardgames_extend.csv', index=False)
-    
-    #get user ratings from page 1
-    for comment in soup.find_all('comment'):
-        user_ratings.append(comment.get('rating'))
-        users.append(comment.get('username'))
-        comments.append(comment.get('value')) 
-        
-    boardgame_id = [ID] * len(user_ratings)
+    if restart == False:
 
-    ratings = pd.DataFrame({
-        'id':boardgame_id,
-        'user':users,
-        'rating':user_ratings,
-        'comment':comments
-    })
+        # extend boardgames dataframe 
+        categories = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgamecategory'})]
+        boardgames.loc[ID,'categories'] = ', '.join(categories)
+        mechanics = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgamemechanic'})]
+        boardgames.loc[ID,'mechanics'] = ', '.join(mechanics)
+        family = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgamefamily'})]
+        boardgames.loc[ID,'family'] = ', '.join(family)
+        expansions = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgameexpansion'})]
+        boardgames.loc[ID,'expansions'] = ', '.join(expansions)
+        integrations = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgameintegration'})]
+        boardgames.loc[ID,'integrations'] = ', '.join(integrations)
+        designers = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgamedesigner'})]
+        boardgames.loc[ID,'designers'] = ', '.join(designers)
+        publishers = [link.get('value') for link in soup.find_all('link', attrs={'type':'boardgamepublisher'})]
+        boardgames.loc[ID,'publishers'] = ', '.join(publishers)
         
-    ratings.to_csv('../data/ratings.csv', index=False, header=None, mode='a')
-    
-    # if more than one page of comments send request for each page
-    if pagecount > page:
-        page = page + 1
+        # update number of voters
+        boardgames.loc[ID, 'num_voters'] = total_items
+        
+        boardgames.to_csv('../data/boardgames_extend.csv', index=False)
+        
+        #get user ratings from page 1
+        for comment in soup.find_all('comment'):
+            user_ratings.append(comment.get('rating'))
+            users.append(comment.get('username'))
+            comments.append(comment.get('value')) 
+            
+        boardgame_id = [ID] * len(user_ratings)
 
-        while page <= pagecount:
-            logging.info(f'start scraping page {page} of {pagecount}')
-            user_ratings = []
-            users = []
-            comments = []
-            ratings = pd.DataFrame()
+        ratings = pd.DataFrame({
+            'id':boardgame_id,
+            'user':users,
+            'rating':user_ratings,
+            'comment':comments
+        })
             
-            api_adress = f"https://www.boardgamegeek.com/xmlapi2/thing?id={ID}&ratingcomments=1&page={page}"
-            response = requests.get(api_adress)
-            logging.info(f'status {response.status_code} for {api_adress}')
-            
-            if response.status_code != 200:
-                logging.warning(f'STATUS {response.status_code}, increase sleep time and try again')
-                time.sleep(10)
-                sleep_default = 5
+        ratings.to_csv('../data/ratings.csv', index=False, header=None, mode='a')
+    
+        # if more than one page of comments send request for each page
+        if pagecount > page:
+            page = page + 1
+
+            while page <= pagecount:
+                logging.info(f'start scraping page {page} of {pagecount}')
+                user_ratings = []
+                users = []
+                comments = []
+                ratings = pd.DataFrame()
                 
-            else:
-            
-                xml = response.text
-                soup = BeautifulSoup(xml, 'xml')
+                api_adress = f"https://www.boardgamegeek.com/xmlapi2/thing?id={ID}&ratingcomments=1&page={page}"
+                response = session.get(api_adress)
+                logging.info(f'status {response.status_code} for {api_adress}')
+                
+                if response.status_code != 200:
+                    logging.warning(f'STATUS {response.status_code}, increase sleep time and try again')
+                    time.sleep(10)
+                    sleep_default = 5
+                    
+                else:
+                
+                    xml = response.text
+                    soup = BeautifulSoup(xml, 'xml')
 
-                for comment in soup.find_all('comment'):
-                    user_ratings.append(comment.get('rating'))
-                    users.append(comment.get('username'))
-                    comments.append(comment.get('value'))
+                    for comment in soup.find_all('comment'):
+                        user_ratings.append(comment.get('rating'))
+                        users.append(comment.get('username'))
+                        comments.append(comment.get('value'))
 
-                boardgame_id = [ID] * len(user_ratings)
+                    boardgame_id = [ID] * len(user_ratings)
 
-                ratings = pd.DataFrame({
-                    'id':boardgame_id,
-                    'user':users,
-                    'rating':user_ratings,
-                    'comment':comments
-                })
+                    ratings = pd.DataFrame({
+                        'id':boardgame_id,
+                        'user':users,
+                        'rating':user_ratings,
+                        'comment':comments
+                    })
 
-                ratings.to_csv('../data/ratings.csv', index=False, header=False, mode='a')
+                    ratings.to_csv('../data/ratings.csv', index=False, header=False, mode='a')
 
-                page = page + 1
-                time.sleep(sleep_default+random.random())
+                    page = page + 1
+                    time.sleep(sleep_default+random.random())
 
+    if restart == True:
+        page = restart_page
+        while page <= pagecount:
+                logging.info(f'start scraping page {page} of {pagecount}')
+                user_ratings = []
+                users = []
+                comments = []
+                ratings = pd.DataFrame()
+                
+                api_adress = f"https://www.boardgamegeek.com/xmlapi2/thing?id={ID}&ratingcomments=1&page={page}"
+                response = session.get(api_adress)
+                logging.info(f'status {response.status_code} for {api_adress}')
+                
+                if response.status_code != 200:
+                    logging.warning(f'STATUS {response.status_code}, increase sleep time and try again')
+                    time.sleep(10)
+                    sleep_default = 5
+                    
+                else:
+                
+                    xml = response.text
+                    soup = BeautifulSoup(xml, 'xml')
+
+                    for comment in soup.find_all('comment'):
+                        user_ratings.append(comment.get('rating'))
+                        users.append(comment.get('username'))
+                        comments.append(comment.get('value'))
+
+                    boardgame_id = [ID] * len(user_ratings)
+
+                    ratings = pd.DataFrame({
+                        'id':boardgame_id,
+                        'user':users,
+                        'rating':user_ratings,
+                        'comment':comments
+                    })
+
+                    ratings.to_csv('../data/ratings.csv', index=False, header=False, mode='a')
+
+                    page = page + 1
+                    time.sleep(sleep_default+random.random())
+        restart = False
