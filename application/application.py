e109442eb48be27cc943e0e51029a2e1cbc207e9
bripui <britta.puyn@outlook.de>
2021-07-07 18:06:58 +0200

    input form at main page and new user form added

--- application/application.py
+++ application/application.py
@@ -1,4 +1,5 @@
 from flask import Flask,  request,  render_template
+from recommender import random_recommender, neighbor_recommender
 
 
 # here we construct a Flack object and the __name__ sets this script as the root
@@ -9,10 +10,19 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
+@app.route('/new_user_form')
+def new_user_form():
+    return render_template('new_user_form.html')
+
+
 @app.route('/recommend')
 def recommend():
-    user_name = request.args.getlist('user_name')
-    return render_template('recommend.html', user_name=user_name)
+    user_name = request.args.getlist('user_name')[0]
+    print(user_name)
+    #recommendations = random_recommender(10)
+    recommendations = neighbor_recommender(user_name)
+    return render_template('recommend.html', user_name=user_name,
+                                            recommendations=recommendations)
 
 
 if __name__ == "__main__":
