656774575f9232370e6855317ecaca8d2630794d
bripui <britta.puyn@outlook.de>
2021-07-08 21:00:28 +0200

    added favorite categories and mechanics to recommend page

--- application/application.py
+++ application/application.py
@@ -1,5 +1,6 @@
 from flask import Flask,  request,  render_template
 from recommender import random_recommender, neighbor_recommender
+from utils import ohe_user_boardgames
 
 
 # here we construct a Flack object and the __name__ sets this script as the root
@@ -21,8 +22,14 @@ def recommend():
     print(user_name)
     #recommendations = random_recommender(10)
     recommendations = neighbor_recommender(user_name)
+    users_top_mechanics = ohe_user_boardgames(user_name, 'mechanics').sum().sort_values(ascending=False)[:5].index.tolist()
+    print(users_top_mechanics)
+    users_top_categories = ohe_user_boardgames(user_name, 'categories').sum().sort_values(ascending=False)[:5].index.tolist()
+
     return render_template('recommend.html', user_name=user_name,
-                                            recommendations=recommendations)
+                                            recommendations=recommendations,
+                                            users_top_mechanics=users_top_mechanics,
+                                            users_top_categories=users_top_categories)
 
 
 if __name__ == "__main__":
