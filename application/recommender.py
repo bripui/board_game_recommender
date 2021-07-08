f76ad95f7725a74009d6e91578ef06ee7af2f762
bripui <britta.puyn@outlook.de>
2021-07-08 20:59:21 +0200

    added ohe_user_boardgames to generate top categories and so on

--- application/recommender.py
+++ application/recommender.py
@@ -22,10 +22,14 @@ def neighbor_recommender(user_name):
     '''
     user_ratings = create_user_ratings(user_name)
     user_vector = create_user_vector(user_name)
-    distances, neighbor_ids = model.kneighbors([user_vector], n_neighbors=20)
+    #create neigbors of user
+    distances, neighbor_ids = model.kneighbors([user_vector], n_neighbors=10)
     neighbor_filter = ratings['user_id'].isin(neighbor_ids[0][1:])
+    #create mean retings of games, rated by the neighbors
     neighbor_ratings = ratings[neighbor_filter].groupby('boardgame_id').mean()
+    #sort rated games by mean rating
     neighbor_top = neighbor_ratings['ratings'].sort_values(ascending=False)
+    #remove games which user rated already
     played_filter = ~neighbor_top.index.isin(user_ratings['boardgame_id'])
     recommend_ids = neighbor_top[played_filter].index
-    return boardgames.loc[recommend_ids]['name'].tolist()
+    return boardgames.loc[recommend_ids]['name'].tolist()[:10]
--- application/utils.py
+++ application/utils.py
@@ -2,15 +2,27 @@ import pandas as pd
 import numpy as np
 
 boardgames = pd.read_csv('../data/boardgames.csv', index_col='id')
+boardgames_ext = pd.read_csv('../data/boardgames_extend_backup.csv')
+boardgames_ext['id'] = boardgames.index
+boardgames_ext = boardgames_ext.set_index('id')
 
-#ratings_small has the correct header! ratings won't
 ratings = pd.read_csv('../data/ratings_cleaned.csv')
+
+users = pd.read_csv('../data/users.csv')
+
 
 def lookup_boardgame(ids):    
     '''
     converts boardgame ids into boardgame names
     '''
     return boardgames.loc[ids, 'name'].tolist()
+
+def lookup_user_id(user_name):
+    '''
+    returns the user id of a user
+    '''
+    user_id = users[users['user_name']==user_name]['user_id'].tolist()[0]
+    return user_id
 
 def create_user_vector(user_name):
     '''
@@ -24,9 +36,43 @@ def create_user_vector(user_name):
     return vector
 
 def create_user_ratings(user_name):
-    user = ratings[ratings['user_name']==user_name]
+    '''
+    returns a dataframe with rated boardgames for a specified user
+    '''
+    user_id = lookup_user_id(user_name)
+    user = ratings[ratings['user_id']==user_id]
     return user
 
+def values_to_list(df, column_name):
+    categories = []
+    for i in df[df[column_name].notna()].iterrows():
+        categories = categories + i[1][column_name].split(', ')
+    categories = list(dict.fromkeys(categories))
+    categories.sort()
+    return categories
 
 
-
+def ohe_user_boardgames(user_name, column, weight=False):
+    '''
+    returns a one-hot-encoded matrix of parameters in column of games played by user
+    if weight = True, the encoding gets weighted by the rating
+    '''
+    games_ohe={}
+    user_id = users[users['user_name']==user_name]['user_id'].tolist()[0]
+    user_ratings = ratings[ratings['user_id']==user_id].set_index('boardgame_id')
+    user_boardgames = boardgames_ext.loc[user_ratings.index]
+    user_boardgames = user_boardgames[user_boardgames[column].notna()]
+    user_categories = values_to_list(user_boardgames, column)    
+    for i in user_boardgames.iterrows():
+        game_vector = [0]*len(user_categories)
+        for c in i[1][column].split(', '):
+            index = user_categories.index(c)
+            if weight == True:
+                game_vector[index]=1 * user_ratings.loc[i[0]]['ratings']
+            else: 
+                game_vector[index]=1
+        games_ohe[i[0]] = game_vector
+    df = pd.DataFrame(games_ohe)
+    df = df.transpose()
+    df.columns = user_categories
+    return df
