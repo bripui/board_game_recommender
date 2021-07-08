3a7327b9b9c7124ee7ce72414d42a5f6a87b74ad
bripui <britta.puyn@outlook.de>
2021-07-08 10:53:19 +0200

    replaced loop with merge, read in only relevant parts of csv

--- scraper/clean_ratings.py
+++ scraper/clean_ratings.py
@@ -13,41 +13,43 @@ def create_users(ratings):
     '''
     returns a user table and creates user ids
     '''
-    users = pd.DataFrame(ratings.value_counts('user_name'), columns=['ratings'])
+    users = pd.DataFrame(ratings.value_counts('user_name'), columns=['num_ratings'])
     user_ids = list(range(0,len(users)))
     users['user_id'] = user_ids
     users.to_csv('../data/users.csv')
     return users
 
-def sort_out_users(ratings, users, n_ratings):
-    '''
-    returns ratings of users who have rated more than n boardgames
-    '''
-    remaining_users = users[users['ratings']>n_ratings]
-    user_filter = ratings['user_name'].isin(remaining_users.index)
-    remaining_ratings = ratings[user_filter]
-    return remaining_ratings
-
 def apply_userids(ratings, users):
-    ratings['user_id'] = None
-    for row in ratings.iterrows():
-        username = row[1]['user_name']
-        user_id = users.loc[username][1]
-        ratings.loc[row[0], 'user_id'] = user_id
-        ratings.drop(columns=['user_name'])
-        print(username, user_id)
+    '''
+    returns a merged dataframe of ratings and users
+    adds user_id and num_ratings to ratings table
+    '''
+    ratings = pd.merge(ratings, users, on='user_name')
     return ratings
+
+def clean_dataframe(ratings, n_ratings):
+    '''
+    returns a cleaned ratings table containing boardgame_ids, user_ids and ratings
+    removes users with less than n ratings
+    '''
+    users = create_users(ratings)
+    ratings = apply_userids(ratings, users)
+    #print(ratings.columns)
+    ratings = ratings[ratings['num_ratings']>=n_ratings]
+    ratings = ratings.drop(columns=['user_name','num_ratings'])
+    return ratings
+
+
 
 if __name__ == "__main__":
 
-    ratings = pd.read_csv('../data/ratings_small.csv')
-    #ratings = pd.read_csv('../data/ratings.csv', 
-    #                       names=['boardgame_id, 'user_name', 'ratings', 'comments']
-    #                       )
+    #ratings = pd.read_csv('../data/ratings_small.csv')
+    ratings = pd.read_csv('../data/ratings_backup.csv', 
+                           usecols=[0,1,2], 
+                           names=['boardgame_id', 'user_name', 'ratings', 'comments']
+                           )
 
-    users = create_users(ratings)
-    ratings_cleaned = sort_out_users(ratings, users, 1)
-    apply_userids(ratings_cleaned, users)
+    ratings_cleaned = clean_dataframe(ratings, 5)
 
     ratings_cleaned.to_csv('../data/ratings_cleaned.csv', index=False)
 
