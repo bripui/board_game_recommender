ecf7574513294359e27a1caa75954fb817abcac4
bripui <britta.puyn@outlook.de>
2021-07-09 19:27:57 +0200

    sql script for creating DB

---
+++ sql/model.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS boardgames CASCADE;
+CREATE TABLE boardgames(
+    bg_index INT,
+    boardgameID INT PRIMARY KEY,
+    boardgameRank FLOAT,
+    boardgameName VARCHAR,
+    boardgameLink VARCHAR,
+    num_voters INT,
+    categories VARCHAR, 
+    machanics VARCHAR, 
+    family VARCHAR,
+    expansions VARCHAR,
+    integrations VARCHAR,
+    designers VARCHAR,
+    publishers VARCHAR
+);
+
+\copy boardgames FROM '../data/boardgames_extend.csv' DELIMITER ',' CSV HEADER;
+
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users(
+  userName VARCHAR,
+  num_ratings INT,
+  userID INT PRIMARY KEY
+);
+
+\copy users FROM '../data/users.csv' DELIMITER ',' CSV HEADER;
+
+DROP TABLE IF EXISTS ratings CASCADE;
+CREATE TABLE ratings(
+  boardgameID INT REFERENCES boardgames,
+  rating FLOAT,
+  userID INT REFERENCES users
+);
+  
+\copy ratings FROM '../data/ratings_cleaned.csv' DELIMITER ',' CSV HEADER;
